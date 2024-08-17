50e346d181e4a5bb5bef956fee6ad9cfd57e7308
Kimwonjoon <kimpass189@gmail.com>
2024-08-17 18:03:39 +0900

    result

--- tests/test_call.py
+++ tests/test_call.py
@@ -1,6 +1,11 @@
 from mvstar.app import mkjson
+from mvstar.mvinfo import read_data
 
 def test_json():
     for i in range(2015 ,2022):
         r = mkjson(dt=i, sleep_time=0.1)
+        assert r
+def test_info():
+    for i in range(2015 ,2022):
+        r = read_data(dt = i, sleep_time = 0.1)
         assert r
